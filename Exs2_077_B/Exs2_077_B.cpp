d6cbf288803407e3dec3f143e20635684273a37c
DzakyHanif19 <dzakyannafi1@gmail.com>
2023-05-12 15:01:39 +0700

    Add project files.

---
+++ Exs2_077_B/Exs2_077_B.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int jaki[109];
+int n;
+int i;
+
+void input() {
+	while (true) {
+		cout << "Masukan nomor element array : ";
+		cin >> n;
+		if ((n > 0) && (n > 109));
+		break;
+		else
+
+	}
+}
